bb40b7829a8b1b250f1983ea0fbec3fb4b0f374f
hitallocavas <hcs3@cin.ufpe.br>
2020-08-08 01:53:51 -0300

    Aplicação Todo List - Versão Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Form, Validators } from '@angular/forms';
+import { Todo } from './models/Todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Lista de Tarefas';
+
+  public todos: Todo[] = [];
+  todoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      title: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(50),
+        ]),
+      ],
+    });
+
+    this.load();
+  }
+
+  public clear(): void {
+    this.todoForm.reset();
+  }
+
+  public add(): void {
+    const title = this.todoForm.controls.title.value;
+    const id = this.todos.length + 1;
+    const todo = new Todo(id, title, false);
+    this.todos.push(todo);
+    this.save();
+    this.clear();
+  }
+
+  public setDone(todo: Todo, value: boolean): void {
+    todo.done = value;
+    this.save();
+  }
+
+  public delete(todo: Todo): void {
+    const index = this.todos.findIndex((el) => el.id === todo.id);
+    this.todos.splice(index, 1);
+    this.save();
+  }
+
+  public save(): void {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  public load(): void {
+    const data = localStorage.getItem('todos');
+    if (data !== null) {
+      this.todos = JSON.parse(data);
+    }
+  }
 }
